45e90058df0ec6169de38db0c320286c97fcdf9d
Work PC <vitos162008@mail.ru>
2023-03-03 17:04:09 +0300

    add

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ let userName = document.querySelector(".profile-user__name");
 let userProfession = document.querySelector(".profile-user__profession");
 
 buttonEdit.addEventListener("click", () => {
-  popup.classList.toggle("popup_open");
+  popup.classList.toggle("popup_opened");
 
   popupUserName.value = userName.textContent;
   popupUserProfession.value = userProfession.textContent;
@@ -19,9 +19,9 @@ popupSubmit.addEventListener("click", (event) => {
   event.preventDefault();
   userName.textContent = popupUserName.value;
   userProfession.textContent = popupUserProfession.value;
-  popup.classList.toggle("popup_open");
+  popup.classList.toggle("popup_opened");
 });
 //test
 popupClose.addEventListener("click", () => {
-  popup.classList.toggle("popup_open");
+  popup.classList.toggle("popup_opened");
 });
